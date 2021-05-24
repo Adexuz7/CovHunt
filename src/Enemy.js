9f417fbb80cf18d93ad8b116e7a29e3b9faa1b9d
Andrés Galván <adexuz7@gmail.com>
2021-05-24 15:12:16 +0100

    turboboosted by alvaro

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,15 +1,26 @@
-function Enemy(left, direction){
+function Enemy (left, direction) {
+  var self = this
   this.left = left
   this.direction = direction
   this.html = 0
-  this.alive = true
-
-  this.create = function(){
-    var canvas = document.getElementById('canvas')
-    var enemy = document.createElement('div')
+  this.id = ''
+  
+  this.create = function (length) {
+    let canvas = document.getElementById('canvas')
+    let enemy = document.createElement('div')
     enemy.classList.add('enemy')
-    enemy.style.left= this.left
+    this.id = `enemy${length}`
+    enemy.setAttribute('id', this.id)
+    enemy.style.left = this.left
     canvas.appendChild(enemy)
+  }
 
+  this.move = function () {
+    self.left += 10 * self.direction
+    self.html.style.left = self.left + 'px'
+  }
+
+  this.die = function () {
+    self.html.parentNode.removeChild(self.html)
   }
 }
--- src/script.js
+++ src/script.js
@@ -5,10 +5,8 @@
 // direction = 1
 
 // let enemyAlive = true
+var enemies = []
 
-var enemy = new Enemy(-200,1)
-enemy.create()
-enemy.html = document.getElementsByClassName('enemy')[0]
 // // ENEMY 2
 // enemyRight = 1100
 // direction2 = -1
@@ -19,42 +17,47 @@ document.getElementById('canvas').addEventListener('click', function () {
   console.log('canvas')
 })
 
+function createEnemy () {
+  var direction = 0
+  var position = 0
+
+  if (Math.random() > 0.5) {
+    direction = 1
+    position = -200
+  } else {
+    direction = -1
+    position = 1000
+  }
+
+  var enemy = new Enemy(position, direction)
+  enemy.create(enemies.length)
+  enemy.html = document.getElementById(`enemy${enemies.length}`)
+  enemy.html.addEventListener('click', killEnemy)
+  enemies.push(enemy)
+}
+
 // DETECTA Y ELIMINA AL ENEMIGO
 function killEnemy (e) {
   e.stopPropagation()
   e.currentTarget.parentNode.removeChild(e.currentTarget)
   console.log('Enemy killed')
+  // Eliminar del array sólo el elemento su id correcta
+
+  clearInterval(timerId)
 }
 
-enemy.html.addEventListener('click', killEnemy)
-
+function moveEnemies() {
+  enemies.forEach(function (enemy) {
+    enemy.move()
+    if (enemy.left > 1000 || enemy.left < -200) {
+      // sacar del array sólo el elemento con la id correcta
+      enemies.pop()
+      enemy.die()
+    }
+  })
+}
 
 // MOVEMENT
+timerId = setInterval(moveEnemies, 50)
 
-const moveBox = function () {
-    enemy.left += 10 * enemy.direction;
-    enemy.html.style.left = enemy.left + 'px';
-  
-    //enemyRight += 10 * - 1 // Cambiar direccion enemy2
-   // enemy2.style.left = enemyRight + 'px' // Move enemy 2
-
-    if (enemy.left > 1000 && !enemy.alive) {
-      
-      enemy.html.parentNode.removeChild(enemy.html);
-      enemy.alive = false
-      console.log('enemigo azul ha desaparecido')
-
-  }
-
-  // Check if enemy 2 shoud be destroyed
-  // if (enemyRight < -300 && enemy2Alive) {
-  //   enemy2.parentNode.removeChild(enemy2)
-  //   enemy2Alive = false
-  //   console.log('enemigo amarillo ha desaparecido')
-  // }
-}
-
-timerId = setInterval(moveBox, 50);
-
-
-
+gameId = setInterval(createEnemy, 1000)
